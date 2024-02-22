5d4c7deeadc8757695566180895fde122afc3665
Lakshan Costa <lakshanrcosta@gmail.com>
2024-02-22 16:51:43 +0200

    Primitive types in typescript

---
+++ section_01/02_premitive_types.ts
@@ -0,0 +1,41 @@
+// Primitive types
+
+/**
+ * Boolean: Represents a boolean value, either true or false.
+ */
+
+const isDone: boolean = false;
+
+/**
+ * Number: Represents both integer and floating-point numbers.
+ */
+const decimal: number = 6;
+const hex: number = 0xf00d;
+const binary: number = 0b1010;
+const octal: number = 0o744;
+const floatingPoint: number = 6.7;
+
+/**
+ * String: Represents textual data, enclosed within single or double quotes.
+ */
+const color: string = 'blue';
+const sentence: string = 'Hello, world!';
+
+/**
+ * Null: Represents the intentional absence of any value.
+ */
+
+const n: null = null;
+
+/**
+ * Undefined: Represents a value that has not been initialized or defined.
+ */
+
+const u: undefined = undefined;
+
+/**
+ * Symbol: Represents a unique and immutable value.
+ */
+
+const sym1: symbol = Symbol('key1');
+const sym2: symbol = Symbol('key2');
